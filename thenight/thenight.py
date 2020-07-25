9ada1aac31afa6c8745f125b8fe4dbae2bc2b9fe
guyinatuxedo <ryancmeinke@gmail.com>
2020-07-25 10:41:59 -0700

    making patch

--- thenight/thenight.py
+++ thenight/thenight.py
@@ -1,6 +1,7 @@
 """Module for Remote Libc Identification"""
 
 import os
+import pickle
 
 INSTALL_DIRECTORY = ""
 
@@ -55,8 +56,8 @@ def find_libc_version(symbol_0: str, addr_0: int, symbol_1: str, addr_1: int):
 	addr_offset = addr_0 - addr_1
 
 	print("Offset:   %s" % hex(addr_offset))
-	print("Symbol_0:  %s" % symbol_0)
-	print("Symbol_1:  %s" % symbol_1)
+	print("Symbol_0:  %s" % str(symbol_0))
+	print("Symbol_1:  %s" % str(symbol_1))
 	print("Address0: %s" % hex(addr_0))
 	print("Address1: %s" % hex(addr_1))
 
@@ -67,3 +68,26 @@ def find_libc_version(symbol_0: str, addr_0: int, symbol_1: str, addr_1: int):
 		libc_offset = look_libc_offset(symbol_0, symbol_1, i)
 		if libc_offset == addr_offset:
 			print("Possible libc: %s" % i)
+
+def find_libc_version_automated(symbol_0: str, addr_0: int, symbol_1: str, addr_1: int):
+	"""Find the all the possible libc matches"""
+
+	# Ensure input values are correct type
+	if not isinstance(addr_0, int) or not isinstance(addr_1, int):
+		raise TypeError("Addresses must be integers.")
+
+	symbol_0 = symbol_type_check(symbol_0)
+	symbol_1 = symbol_type_check(symbol_1)
+
+	# Calculate what the offset for the symbols will be
+	addr_offset = addr_0 - addr_1
+
+	# Iterate through all of the symbol files
+	# Checking to see which ones are possible matches
+	output_file = open("TheNight-Out", "wb")
+	files = os.listdir(INSTALL_DIRECTORY + "symbols/")
+	for i in files:
+		libc_offset = look_libc_offset(symbol_0, symbol_1, i)
+		if libc_offset == addr_offset:
+			pickle.dump(i, output_file)
+	output_file.close()
